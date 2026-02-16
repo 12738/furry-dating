b89ed3d79e0f72066b2cfffcd8039e8a8ae4d4ef
M_Snepsi <joewsalerno@gmail.com>
2026-02-16 14:51:14 -0500

    images updated

--- furry dating/Assets/Scripts/Dialogue/DialogueText.cs
+++ furry dating/Assets/Scripts/Dialogue/DialogueText.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,7 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Dialogue/New Dialogue Container")]
 public class DialogueText : ScriptableObject
 {
+    [Range(0, 2)] public int SpriteIndex;
     public string speakerName;
     [TextArea(5, 10)]
     public string[] paragraphs;
